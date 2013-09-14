ae3d6fb0361bb5a6fa345713cd360c0a826667bc
Josh Matcuk <jmatcuk@gmail.com>
2013-09-14 14:11:31 -0500

    Likely going to give up on this due to script block argument passing losing type.

--- ConvertWtvToDvr-MSConcurrently.ps1
+++ ConvertWtvToDvr-MSConcurrently.ps1
@@ -1,12 +1,24 @@
 ﻿# Original script copied from http://wannemacher.us/?p=412
+######################################################
+## Configuration Variables                          ##
+######################################################
+
+# Define the location of Windows WTVConverter.exe
+$wTvConverter = "C:\Windows\ehome\WTVConverter.exe"
+
+# Define the location of recorded (*.wtv) shows.
+$recordedTv = "T:\Recorded TV"
+
+# The buffer time in minutes that you want any actions on files to be delayed
+#  in order to ensure that they are currently being recorded or written to.
+$writeBufferMinutes = 5
+
 Write-Host "Script Starting."
+
+$recordedShows = @(Get-ChildItem ($recordedTv + '\' + '*.wtv') | Where-Object {$_.LastWriteTime -lt (get-date).AddMinutes(-$writeBufferMinutes)})
+
+$waitTime = 10000 # Duration in ms the script waits to check for job status changes.
  
-$concurrentTasks = 3 # How many tasks can run at once.
-$waitTime = 500 # Duration in ms the script waits to check for job status changes.
- 
-# $jobs holds pertinent job information. This is a test script so these are
-# just simple values to track script progress.
-$jobs = @("a", "b", "c", "d", "e", "f", "g")
 $jobIndex = 0 #Which job is up for running
 $workItems = @{} # Items being worked on
 $workComplete = $false # Is the script done with what it needs to do?
@@ -24,15 +36,52 @@ while (!$workComplete) {
 	}
  
 	# Start new jobs if there are open slots.
-	while (($workItems.Count -lt $concurrentTasks) -and 
-		   ($jobIndex -lt $jobs.Length)) {
-			# These jobs don't do anything other than wait a variable amount of time
-			# and print an output message.
-			$workTime = Get-Random -Minimum 2000 -Maximum 10000
-			$job = $jobs[$jobIndex]
-			"Starting job {0}." -f $job
-			$workItems[$job] = Start-Job -ArgumentList $workTime, $job -ScriptBlock {Start-Sleep -Milliseconds $args[0]; "{0} processed." -f $args[1]}
-			$jobIndex += 1
+	while (($workItems.Count -lt $recordedShows.Length) -and ($jobIndex -lt $recordedShows.Length)) {
+		$job = $recordedShows[$jobIndex]
+        #echo ($job.GetType().ToString())
+
+        "Starting job {0}." -f $job.Name
+        $workItems[$job] = Start-Job -ArgumentList @($job) -ScriptBlock {
+            $show = [System.IO.FileInfo] $args[0]
+            
+            #Start-Sleep -Milliseconds $args[0]; "{0} processed." -f $args[1]
+            $outputFileName = $args[0].DirectoryName + '\' + $args[0].BaseName + '.dvr-ms'
+            echo $outputFileName
+            # If the output file does not already exist.
+            if (!(Test-Path $outputFileName))
+            {
+                echo ($outputFileName + " does not already exists.")
+
+                # Start conversion.
+                &$wTvConverter $args[0].FullName $outputFileName
+            
+                # The executible from Microsoft does not operate synchonously or output an error code
+                #  so pause 10 seconds to see if a file is created.
+                Start-Sleep 10 #seconds
+
+                # We want to verify that the file exists because if it doesn't then it is likely copy-protected.
+                if (Test-Path $outputFileName)
+                {
+                    do
+                    {
+                        # Check every minute if it's finished being touched.
+                        Start-Sleep 60 #seconds
+                        $outputFile = Get-ChildItem $outputFileName
+                    }
+                    while($outputFile.LastWriteTime -gt (get-date).AddMinutes(-$writeBufferMinutes))
+                    # Remove the original .wtv file once it hasn't been written to in the last 5 minutes.
+                    Remove-Item $args[0].FullName
+                }
+                else
+                {
+                    echo ($args[0].BaseName + " must be copy-protected.")
+                }
+            }
+        }
+			
+
+
+        $jobIndex += 1
 	}
  
 	# If all jobs have been processed we are done.
