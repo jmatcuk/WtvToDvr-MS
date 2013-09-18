9639f7d9e2fbee8c3bfd56235250c6c29ea65921
Josh Matcuk <jmatcuk@gmail.com>
2013-09-17 22:41:41 -0500

    Finished documenting Convert All script and configuring debug messages to use $DebugPreference.

--- ConvertAllWtvToDvr-MS.ps1
+++ ConvertAllWtvToDvr-MS.ps1
@@ -2,7 +2,15 @@
 ## Configuration Variables                          ##
 ################################################### #>
 
-# Define the location of Windows WTVConverter.exe
+<#
+$DebugPreference = "Continue" PowerShell will show the debug message.
+$DebugPreference = "SilentlyContinue" PowerShell will not show the message.
+$DebugPreference = "Stop" PowerShell will show the message and then halt.
+$DebugPreference = "Inquire" PowerShell will prompt the user.
+#>
+$DebugPreference = "SilentlyContinue"
+
+# Define the location of Windows WTVConverter.exe.
 $wTvConverter = "C:\Windows\ehome\WTVConverter.exe"
 
 # The buffer time in minutes that you want any actions on files to be delayed
@@ -15,7 +23,10 @@ $convertedShowFileExtension = ' - DVRMS.dvr-ms'
 
 # Define the location of recorded ($recordingsFileExtensionPattern) shows.
 $recordedTv = "T:\Recorded TV"
+# Location to move files to when not deleting them.
 $deletedShows = "T:\DeletedShows"
+# Move or delete the file?  True to Move to $deletedShows or False to delete.
+$moveToDeletedShows = $true
 
 # Is the script done with what it needs to do?
 $workComplete = $false
@@ -32,24 +43,23 @@ Function GetOutputFileName ($file) { ([System.IO.FileInfo] $file).DirectoryName 
 ######################################################
 # This script will loop through all discovered *.wtv #
 # in a location where recorded tv is stored.  Any    #
-# file that has not been touched in the last 5       #
+# file that has not been touched in the last buffer  #
 # minutes will converted to .dvr-ms if a matching    #
 # .dvr-ms file is not found.  After the file has     #
-# completed being written to for 5 minutes, it will  #
-# be deleted from the system.                        #
+# completed being written to for buffer minutes, it  #
+# will be deleted from the system.                   #
 ################################################### #>
 
 # Retrieve all of the recorded ($recordingsFileExtensionPattern) shows that have not already been converted or touched within the last $writeBufferMinutes.
 $showsToConvert = [System.Collections.ArrayList] (Get-ChildItem ($recordedTv + '\' + $recordingsFileExtensionPattern) |
     Where-Object {$_.LastWriteTime -lt (get-date).AddMinutes(-$writeBufferMinutes)} |
-    Where-Object {!(Test-Path ($_.DirectoryName + '\' + $_.BaseName + '.dvr-ms'))} | # Converted by WTVConverter.exe
     Where-Object {!(Test-Path ($_.DirectoryName + '\' + $_.BaseName + $convertedShowFileExtension))}) # Converted by Right-Click Convert to .dvrms
 
 # Exit if no shows are ready for conversion.
-if ($showsToConvert.Count -eq 0) {echo ("No recordings found."); exit 0}
+if ($showsToConvert.Count -eq 0) {Write-Debug ("No recordings found."); exit 0}
 
 # At least one show is ready for conversion.
-("Show count: " + $showsToConvert.Count) # Debug
+Write-Debug ("Show count: " + $showsToConvert.Count) # Debug
 
 $showsToConvert | ForEach-Object {
     # Casting variable type.
@@ -57,7 +67,7 @@ $showsToConvert | ForEach-Object {
 
     # create output file name.
     $outputFileName = GetOutputFileName $recording
-    ("Converting " + $recording.Name + " to " + $recording.BaseName + $convertedShowFileExtension)
+    Write-Debug ("Converting " + $recording.Name + " to " + $recording.BaseName + $convertedShowFileExtension)
 
     # Start conversion.
     &$wTvConverter $recording.FullName $outputFileName
@@ -77,12 +87,12 @@ $showsToConvert | ForEach-Object {
     $outputFileName = $recording.DirectoryName + '\' + $recording.BaseName + $convertedShowFileExtension
     
     # Store files that did not create output files.
-    if (!(Test-Path $outputFileName)) {($recording.Name + " must be copy protected."); $null = $copyProtectedShows.Add($recording)}
+    if (!(Test-Path $outputFileName)) {Write-Debug ($recording.Name + " must be copy protected."); $null = $copyProtectedShows.Add($recording)}
 }
 $copyProtectedShows | ForEach-Object {$showsToConvert.Remove($_)} # Remove copy-protected shows.
 
 # Exit if all shows are copy protected.
-if ($showsToConvert.Count -eq 0) {echo ("All shows found were copy protected."); exit 0}
+if ($showsToConvert.Count -eq 0) {Write-Debug ("All shows found were copy protected."); exit 0}
 
 while (!$workComplete) {
     # Check every minute if each conversion has completed by at least #writeBufferMinutes.
@@ -98,24 +108,28 @@ while (!$workComplete) {
 
         # Since the existing variable doesn't contain the latest time, we need to re-query for it.
         $outputFile = Get-ChildItem $outputFileName
-        #echo ("OutputFile: " + $outputFile.Name)
-        #echo ("Last Write Time: " + $outputFile.LastWriteTime)
-        #echo ("Now minus " + $writeBufferMinutes + " minutes: " + (get-date).AddMinutes(-$writeBufferMinutes))
+        Write-Debug ("OutputFile: " + $outputFile.Name)
+        Write-Debug ("Last Write Time: " + $outputFile.LastWriteTime)
+        Write-Debug ("Now minus " + $writeBufferMinutes + " minutes: " + (get-date).AddMinutes(-$writeBufferMinutes))
         if ($outputFile.LastWriteTime -le (get-date).AddMinutes(-$writeBufferMinutes)) {
+            Write-Debug ("Remove Item: " + $recording.FullName)
+
             # Remove the original .wtv file once it hasn't been written to in the last $writeBufferMinutes.
-            #Remove-Item $_.FullName
-            Move-Item $recording.FullName $deletedShows #Move it temporarily until I know everything works.
+            if ($moveToDeletedShows) {
+                Move-Item $recording.FullName $deletedShows
+            } else {
+                Remove-Item $recording.FullName
+            }
 
-            echo ("Remove Item: " + $recording.FullName)
-
-            # Add show to remove to queue.
+            # Add show that will be removed to queue of finished shows.
             $null = $finishedShows.Add($recording)
-            #$showsToConvert.Remove($recording)
         }
     }
-    $finishedShows | ForEach-Object {$showsToConvert.Remove($_)} # Remove finished shows.
+    # Remove finished shows.
+    $finishedShows | ForEach-Object {$showsToConvert.Remove($_)}
+
     if ($showsToConvert.Count -le 0) {
         $workComplete = $true;
     }
-    ("Show count: " + $showsToConvert.Count) # Debug
+    Write-Debug ("Show count: " + $showsToConvert.Count) # Debug
 }
